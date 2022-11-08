12ff225d0b865e69fc6b6c15bb2f945c3e21b761
Phil Yeeles <Phil.Yeeles@ons.gov.uk>
2022-11-08 15:03:28 +0000

    added placeholder functions for each step

--- rbeis.py
+++ rbeis.py
@@ -12,11 +12,39 @@ test_data = pd.read_csv("../understanding/data.csv")
 impvar = "white"
 auxvars = ["interim_id", "gor9d", "work_status_group", "dvhsize"]
 
+def _prep(data):
+    pass
 
-def impute(*, data: int, impute_var: int, aux_vars: int) -> int:
-    return data + impute_var + aux_vars
+def _assign_igroups(data, aux_vars):
+    pass
 
+def _calc_distances(data, dist_func):
+    pass
 
-t = [1,
- 2,
- 3]
+def _get_donors(igroup):
+    pass
+
+def _get_freq_dist(igroup):
+    pass
+
+def _freq_to_exp(freq_dist):
+    pass
+
+def impute(*, data, impute_var, aux_vars, weights, dist_func, min_margin, overwrite):
+    # data: dataframe
+    # impute_var: string
+    # aux_vars: string list
+    # weights: (string * float) dict
+    # dist_func: numeric * numeric -> numeric function
+    # min_margin: float
+    # overwrite: bool
+    # - (check that weights keys correspond to aux_vars)
+    # - prep data
+    # - assign igroups
+    # - calculate distances
+    # - get donors
+    # - get igroup freq dists
+    # - convert to rbeis donor pools
+    # - assign imputed variables
+    # - return dataset
+    pass
