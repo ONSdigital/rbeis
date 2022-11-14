64db4f401262152a3998938d08a06bbe84c09d7e
Phil Yeeles <Phil.Yeeles@ons.gov.uk>
2022-11-14 14:44:43 +0000

    implemented donor identification but TODO need to check if indices correspond to igroups

--- rbeis.py
+++ rbeis.py
@@ -48,6 +48,7 @@ _df1 = lambda x, y: int(x != y)
 _df2 = lambda x, y, m: int(abs(x - y) > m)
 _df3 = lambda x, y, m: 1 if abs(x - y) > m else abs(x - y) / (m + 1)
 
+
 def _get_igroup_aux_var(data, aux_var):
     """
     data (pd.DataFrame): The dataset undergoing imputation
@@ -65,6 +66,7 @@ def _get_igroup_aux_var(data, aux_var):
         )
     return out
 
+
 def _build_custom_df(dist_func, mappings):
     """
                         dist_func (int): Which of the six standard distance
@@ -77,14 +79,21 @@ def _build_custom_df(dist_func, mappings):
     Return a two-argument function that corresponds to distance functions 1, 2
     or 3, but with certain pairs of values overridden.
     """
-    assert dist_func >=4 and dist_func <= 6
-    if dist_func==4: df=_df1
-    elif dist_func==5: df=_df2
-    elif dist_func==6: df=_df3
-    else: raise Exception("You may only choose 4, 5 or 6 for a custom distance function.")
-    return lambda x,y: mappings[(x,y)] if (x,y) in mappings.keys() else df(x,y)
+    assert dist_func >= 4 and dist_func <= 6
+    if dist_func == 4:
+        df = _df1
+    elif dist_func == 5:
+        df = _df2
+    elif dist_func == 6:
+        df = _df3
+    else:
+        raise Exception("You may only choose 4, 5 or 6 for a custom distance function.")
+    return lambda x, y: mappings[(x, y)] if (x, y) in mappings.keys() else df(x, y)
 
-def _calc_distances(data, aux_vars, dist_func, weights, threshold=None, custom_df_map=None):
+
+def _calc_distances(
+    data, aux_vars, dist_func, weights, threshold=None, custom_df_map=None
+):
     """
                          data (pd.DataFrame): The dataset undergoing imputation
                          aux_vars (str list): The names of the chosen auxiliary
@@ -123,7 +132,11 @@ def _calc_distances(data, aux_vars, dist_func, weights, threshold=None, custom_d
     # Check that all of the auxiliary variables have weights specified
     assert all(map(lambda v: v in aux_vars, weights.keys()))
     # Calculate the distances
-    dist_func = _build_custom_df(dist_func,custom_df_map) if dist_func>=4 else [_df1, _df2, _df3][dist_func - 1]
+    dist_func = (
+        _build_custom_df(dist_func, custom_df_map)
+        if dist_func >= 4
+        else [_df1, _df2, _df3][dist_func - 1]
+    )
     igroup_aux_vars = []
     vars_vals = zip(aux_vars, map(lambda v: _get_igroup_aux_var(data, v), aux_vars))
     for i in range(0, data["IGroup"].max()):
@@ -148,8 +161,30 @@ def _calc_distances(data, aux_vars, dist_func, weights, threshold=None, custom_d
     del data["dists_temp"]
 
 
-def _get_donors(igroup):
-    pass
+def _get_donors(data, min_quantile=None):
+    """
+    data (pd.DataFrame): The dataset undergoing imputation
+     min_quantile (int): Instead of choosing the minimum distance, choose
+                         records in the lowest n-quantile, where min_quantile
+                         specifies n (e.g. if min_quantile is 10, then accept
+                         records in the bottom decile).
+
+    Add a column 'donor' containing a list of IGroup numbers to which each
+    record is a donors.
+    1. Calculate the distances less than or equal to which a record may be
+       accepted as a donor to each respective IGroup.
+    2. Zip each record's distances to the list of distances calculated in step
+       1, and identify where the record distances are less than or equal to the
+       maximum required IGroup distances, giving a list of indices where this is
+       the case.
+    """
+    igroups_dists = np.array(data.query("not(impute)")["distances"].values.tolist()).T.tolist()
+    igroups_dists.sort()
+    # This would be a lot nicer if we could have NumPy >= 1.15.0, which has
+    # np.quantile, but we're on 1.13.0
+    max_donor_dists = list(map((lambda l: l[int(np.ceil(len(l)/n))-1]) if min_quantile else min,igroups_dists)
+    data["donor"] = data.apply(lambda r: np.where(list(map(lambda p: p[0]<=p[1],zip(r["distances"],max_donor_dists))))[0],tolist() if not(r["impute"]) else [], axis=1)
+    # TODO: DO WE HAVE AN OFF-BY-ONE ERROR HERE?  IGROUP NUMBERS (from 1) OR LIST INDICES (from 0)?
 
 
 def _get_freq_dist(igroup):
@@ -161,14 +196,14 @@ def _freq_to_exp(freq_dist):
 
 
 def impute(
-    data, impute_var, aux_vars, weights, dist_func, min_margin, overwrite, col_name=None
+    data, impute_var, aux_vars, weights, dist_func, min_quantile=None, overwrite=False, col_name=None
 ):
     # data: dataframe
     # impute_var: string
     # aux_vars: string list
     # weights: (string * float) dict
     # dist_func: numeric * numeric -> numeric function
-    # min_margin: float
+    # min_quantile: int
     # overwrite: bool
     # col_name: string
     # - (check that weights keys correspond to aux_vars)
@@ -187,6 +222,7 @@ def impute(
 test_data = pd.read_csv("../understanding/data.csv")
 test_imp_var = "white"
 test_aux_vars = ["interim_id", "gor9d", "work_status_group", "dvhsize"]
+test_df = 1
 test_weights = {k: 1.0 for k in test_aux_vars}
 
 
@@ -199,4 +235,4 @@ def t2():
 
 
 def t3():
-    _calc_distances(test_data, test_aux_vars, 1, test_weights)
+    _calc_distances(test_data, test_aux_vars, test_df, test_weights)
