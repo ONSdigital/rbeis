a9469a91b27eaba16fd4f979258561878418d1cc
Phil Yeeles <Phil.Yeeles@ons.gov.uk>
2022-12-08 15:25:16 +0000

    custom mappings _build_custom_df are now asymmetric (updated docstrings to reflect this); broke test cruft out into tests.py, which will need replacing with a proper pytest setup when I come back in January

--- rbeis.py
+++ rbeis.py
@@ -83,7 +83,9 @@ def _build_custom_df(dist_func, mappings, threshold=None):
                                          5 or 6 are permissible)
     mappings (('a * 'a) * numeric dict): A dictionary with pairs of values to be
                                          overridden as its keys, and the desired
-                                         distances as the corresponding values
+                                         distances as the corresponding values.
+                                         Note that (x,y) -> z does not imply
+                                         that (y,z) -> z.
                     threshold (numeric): [Optional] A threshold value for the
                                          distance function, if required
 
@@ -104,8 +106,6 @@ def _build_custom_df(dist_func, mappings, threshold=None):
         return (
             lambda x, y: mappings[(x, y)]
             if (x, y) in mappings.keys()
-            else mappings[(y, x)]
-            if (y, x) in mappings.keys()
             else df(x, y)
         )
     else:
@@ -113,8 +113,6 @@ def _build_custom_df(dist_func, mappings, threshold=None):
         return (
             lambda x, y: mappings[(x, y)]
             if (x, y) in mappings.keys()
-            else mappings[(y, x)]
-            if (y, x) in mappings.keys()
             else df(x, y, threshold)
         )
 
@@ -140,7 +138,8 @@ def _calc_distances(
                                               pairs (2-tuples) of values to
                                               distances, overriding the
                                               underlying standard distance
-                                              function
+                                              function.  Note that (x,y) -> z
+                                              does not imply that (y,z) -> z.
 
     Add a column '_distances' containing lists of calculated distances of each
     record's auxiliary variables from those of its IGroup:
@@ -384,7 +383,8 @@ def impute(
                                               pairs (2-tuples) of values to
                                               distances, overriding the
                                               underlying standard distance
-                                              function
+                                              function.  Note that (x,y) -> z
+                                              does not imply that (y,z) -> z.
                           min_quantile (int): [Optional] Instead of choosing the
                                               minimum distance, choose records
                                               in the lowest n-quantile, where
@@ -465,59 +465,3 @@ def impute(
         del data["_IGroup"]
         del data["_distances"]
         del data["_donor"]
-
-
-# Test setup: same dataset as in the notebook example
-test_data = pd.read_csv("/home/cdsw/rbeis-understanding/data.csv")
-test_imp_var = "white"
-test_aux_vars = ["interim_id", "gor9d", "work_status_group", "dvhsize"]
-test_df = 1
-test_weights = {k: 1.0 for k in test_aux_vars}
-
-
-def t1():
-    _add_impute_col(test_data, test_imp_var)
-
-
-def t2():
-    _assign_igroups(test_data, test_aux_vars)
-
-
-def t3():
-    _calc_distances(test_data, test_aux_vars, test_df, test_weights)
-
-
-def t4():
-    _calc_donors(test_data)
-
-
-def t4a():
-    t1()
-    t2()
-    t3()
-    t4()
-    return test_data
-
-
-def t5(i):
-    return _impute_igroup(
-        test_data,
-        _freq_to_exp(test_data, _get_freq_dist(test_data, "white", [0, 1], i), i),
-        [0, 1],
-        i,
-    )
-
-
-def t6():
-    impute(
-        test_data,
-        "dvsex",
-        [1, 2],
-        ["interim_id", "uac", "hh_id_fake"],
-        {"interim_id": 1, "uac": 2, "hh_id_fake": 3},
-        6,
-        threshold=1,
-        custom_df_map={(1, 1): 2},
-        min_quantile=4,
-        keep_intermediates=True,
-    )
